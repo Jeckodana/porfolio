ab102a02cbed48fd67b83cbaf0b8588916fddd75
JesicaDeluret <jesicadeluretgmail.com.ar>
2022-06-14 20:58:10 -0300

    creacion componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProComponent } from './components/logo-arg-pro/logo-arg-pro.component';
+import { RedessocialesComponent } from './components/redessociales/redessociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProComponent,
+    RedessocialesComponent
   ],
   imports: [
     BrowserModule,
